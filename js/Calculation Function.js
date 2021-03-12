1307f3843e9f25c38551cfc4b0e25199221539e2
Lakshraj <70052551+lakshrajj@users.noreply.github.com>
2021-03-12 11:41:42 +0530

    Update Calculation Function.js

--- js/Calculation Function.js
+++ js/Calculation Function.js
@@ -1,26 +1,27 @@
 function calculate(num1,func,num2){
+  var ans;
 
 switch (func) {
-               case '+': document.write(num1+num2);
+               case '+': ans=(num1+num2);
                break;
             
-               case '-': document.write(num1-num2);
+               case '-':ans=(num1-num2);
                break;
             
-               case '*': document.write(num1*num2);
+               case '*': ans=(num1*num2);
                break;
             
-               case '/': document.write(num1/num2);
+               case '/': ans=(num1/num2);
                break;
-               default:  document.write("Wrong Input Error <br />")
+               default: ans=("Wrong Input Error <br />")
             }
-
+return ans;
 }
 
 var a = prompt("Enter Number ","0");
 var x =prompt("Enter Funtion to Perform ","0");
 var b = prompt("Enter 2nd Number ","0");
 
-calculate(a,x,b);
+ document.write(calculate(a,x,b));
 
 //Lakshrajj
