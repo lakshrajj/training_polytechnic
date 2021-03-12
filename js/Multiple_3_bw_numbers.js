a33fb55d679cbf0709f50e02e3582d1f285efdb9
Lakshraj <70052551+lakshrajj@users.noreply.github.com>
2021-03-12 22:42:06 +0530

    Create Multiple_3_bw_numbers.js
    Function to Find Multiples of 3 between 2 Numbers.

---
+++ js/Multiple_3_bw_numbers.js
@@ -0,0 +1,11 @@
+function calcs(num1,num2){          //func to print multiple of 3
+for (num1; num1 <= num2; num2--) {           //main  loop
+  if(num2%3===0){                            //Base Condition 
+    console.log(num2)
+  }
+ }
+}
+calcs(10,200);                                //Any to Numbers
+
+//This one is Best i Code i can think of for this Program...
+//this is like version 3...lol
