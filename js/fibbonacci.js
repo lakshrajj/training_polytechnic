2e8bfe9ddb56ee3930481628e2cc9af303293322
Lakshraj <70052551+lakshrajj@users.noreply.github.com>
2021-03-16 21:14:07 +0530

    Create fibbonacci.js

---
+++ js/fibbonacci.js
@@ -0,0 +1,8 @@
+a=1;b=1; console.log(a)
+for(i=1;i<=10;i++){               // Fibbonacci Series Between
+     c=a+b                      //  1 to 10. Taking First and Second no
+     a=b                      //  1 and 2 respectively..    ;)
+     b=c
+  if(c<11)
+     console.log(c)
+}
