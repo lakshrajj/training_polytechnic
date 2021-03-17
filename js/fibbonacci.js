ec47242306345e47e40f89a33dab7162a7eb4ac2
Lakshraj <70052551+lakshrajj@users.noreply.github.com>
2021-03-17 10:40:07 +0530

    Update fibbonacci.js

--- js/fibbonacci.js
+++ js/fibbonacci.js
@@ -1,5 +1,5 @@
 // Fibbonacci Series Between 1 to 10. Taking First and Second no 1 and 2 respectively..    ;)
-a=1;b=1; console.log(a)
+a=0;b=1;
 for(i=1;i<=10;i++){               
      c=a+b                      
      a=b                      
