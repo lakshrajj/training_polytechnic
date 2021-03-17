bc3457556b5d8b3aaaaf0fdacf6655a11b6e3bf2
Lakshraj <70052551+lakshrajj@users.noreply.github.com>
2021-03-17 13:22:03 +0530

    Update largestprimefactor.js

--- js_extra/largestprimefactor.js
+++ js_extra/largestprimefactor.js
@@ -1,11 +1,23 @@
 // 600851475143 largest prmie factor
-a=600851475143 
+function prime(num){
+	for (i=2;i<num/2;i++){
+		if(num%i===0){
+			return undefined;
+		}
+	}
+	return num;
+}
+
+a=6008
 c=0
 for (x=2;x<=a-1;x++){
     if(a%x===0){
-      c=x
+      if(prime(x)){
+      	c=x
+      }
     } 
   }
 
 console.log(c)
-// not done yet
+
+// not confirmedd
