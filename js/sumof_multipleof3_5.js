c2144ecfa5b0013beeec3f7137533cca831c9fe3
Lakshraj <70052551+lakshrajj@users.noreply.github.com>
2021-03-17 10:41:18 +0530

    Update sumof_multipleof3_5.js

--- js/sumof_multipleof3_5.js
+++ js/sumof_multipleof3_5.js
@@ -1,6 +1,7 @@
-var a=0                             //  Sum of All Natural Number
-for (i=1;i<1000;i++){               //  which are multiple of 3 or 5
-     if(i%3===0 || i%5===0){        //   below 1000  
+   //  Sum of All Natural Number  which are multiple of 3 or 5  below 1000  
+var a=0
+for (i=1;i<1000;i++){
+     if(i%3===0 || i%5===0){
         a=a+i;
        }
     }
