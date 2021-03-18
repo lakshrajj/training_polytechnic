8c2573f483a0d1325c3a69826f55bb39ad34a4df
Lakshraj <70052551+lakshrajj@users.noreply.github.com>
2021-03-18 10:55:57 +0530

    Update problem5.js

--- js_extra/problem5.js
+++ js_extra/problem5.js
@@ -1,16 +1,16 @@
 //smallest positive number that is evenly divisible by all of the numbers from 1 to 20
-function lar(i,num){
-
-	if(i%num===0){
-	if (num<21){
-		
-		lar(i,num+1)
-	} else {   console.log(i);   }
-
-	}
-}
-
-for (i=2;i<300000000;i++){
+for (i=2;i>1;i++){
 lar(i,2)
 }
+	function lar(j,num){
+
+	if(j%num===0){
+	if (num<21){
 		
+		lar(j,num+1)
+	   	} else {   console.log(i)
+		        		i=0;
+		       }
+
+}}
+	
