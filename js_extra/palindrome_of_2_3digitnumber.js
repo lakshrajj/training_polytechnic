cb1675175a77632d7ab246481944485359246bc9
Lakshraj <70052551+lakshrajj@users.noreply.github.com>
2021-03-18 09:03:49 +0530

    Create palindrome_of_2_3digitnumber.js

---
+++ js_extra/palindrome_of_2_3digitnumber.js
@@ -0,0 +1,14 @@
+l=0
+for(i = 100; i <= 999; i++){
+	for(j = 100; j <= 999; j++){
+		if(i*j == (i*j).toString().split('').reverse().join('')){
+			k=i*j ;
+			if(k>l){l=k}
+			
+		}
+	}
+}
+
+console.log(l)
+
+//Working..
