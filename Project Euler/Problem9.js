dbc358b7ff654cfd69772edc9cc41e301016c11f
Lakshraj <70052551+lakshrajj@users.noreply.github.com>
2021-03-19 12:47:32 +0530

    Create Problem9.js

---
+++ Project Euler/Problem9.js
@@ -0,0 +1,15 @@
+//one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+for (a = 1; a < 350; a++) {
+   sum = 0;
+  for ( b = a + 1; sum <= 1000; b++) {
+     c = Math.sqrt((a * a) + (b * b));
+    sum = a + b + c;
+    if (sum === 1000) {
+    	console.log("Sum of A+b+c=")
+    	console.log(a + b + c)
+      console.log(a * b * c);
+    }
+  }
+}
+//Working Fine Executing in less than 1 sec
